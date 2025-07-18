a2c28237bec27311b1c10d24378f9c3a34da5bd6
FunkinCreatorX <lfurry583@gmail.com>
2025-07-17 23:42:39 -0500

    TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -548,11 +548,11 @@ class TitleState extends MusicBeatState
 				case 13:
 					deleteCoolText();
 				case 14:
-					addMoreText('Friday');
+					addMoreText('Vs GiggleLand');
 				case 15:
-					addMoreText('Night');
+					addMoreText('Amazing');
 				case 16:
-					addMoreText('Funkin'); // credTextShit.text += '\nFunkin';
+					addMoreText('Theme Park!!'); // credTextShit.text += '\nFunkin';
 
 				case 17:
 					skipIntro();
